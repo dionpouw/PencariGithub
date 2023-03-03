aee28d3b4723f084a41e80a697ac480815735a40
dionpouw <aldion.sumampouw@gmail.com>
2023-03-03 17:08:41 +0800

    Setup homefragment and it's recyclerview

---
+++ app/src/main/java/com/jeflette/pencarigithub/ui/homeFragment/HomeFragment.kt
@@ -0,0 +1,80 @@
+package com.jeflette.pencarigithub.ui.homeFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jeflette.pencarigithub.databinding.FragmentHomeBinding
+import com.jeflette.pencarigithub.ui.adapter.GithubAccountAdapter
+import com.jeflette.pencarigithub.utils.Resource
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+
+    private val viewModel: HomeViewModel by viewModels()
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val userAdapter = GithubAccountAdapter()
+
+        binding.rvUsers.apply {
+            adapter = userAdapter
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(false)
+        }
+        userAdapter.notifyDataSetChanged()
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    viewModel.getGithubUser(query)
+                    lifecycleScope.launchWhenStarted {
+                        viewModel.users.collectLatest {
+                            when (it) {
+                                is Resource.Success -> {
+                                    binding.rvUsers.visibility = View.VISIBLE
+                                    binding.progressBar.visibility = View.GONE
+                                    it.data?.items?.let { it1 -> userAdapter.setUserList(it1) }
+                                }
+                                is Resource.Error -> {
+                                    binding.rvUsers.visibility = View.GONE
+                                    binding.progressBar.visibility = View.GONE
+                                }
+                                is Resource.Loading -> {
+                                    binding.rvUsers.visibility = View.GONE
+                                    binding.progressBar.visibility = View.VISIBLE
+                                }
+                            }
+                        }
+                    }
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        _binding = null
+    }
+}
