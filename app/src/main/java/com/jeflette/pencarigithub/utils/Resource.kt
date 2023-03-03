65149086878a97efd30c96ff73a0ee41a6d4189d
dionpouw <aldion.sumampouw@gmail.com>
2023-03-03 17:34:38 +0800

    remove empty loading constructor

--- app/src/main/java/com/jeflette/pencarigithub/utils/Resource.kt
+++ app/src/main/java/com/jeflette/pencarigithub/utils/Resource.kt
@@ -4,6 +4,6 @@ sealed class Resource<T>(
     val data: T? = null, val error: Throwable? = null
 ) {
     class Success<T>(data: T) : Resource<T>(data)
-    class Loading<T>() : Resource<T>()
+    class Loading<T> : Resource<T>()
     class Error<T>(data: T? = null, throwable: Throwable) : Resource<T>(data, throwable)
 }
