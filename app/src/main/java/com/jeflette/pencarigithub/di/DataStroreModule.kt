5b2566dee452ecbdaebfd37268e894b21a6de3d7
dionpouw <aldion.sumampouw@gmail.com>
2023-03-13 13:49:26 +0800

    injection for datastore

---
+++ app/src/main/java/com/jeflette/pencarigithub/di/DataStroreModule.kt
@@ -0,0 +1,30 @@
+package com.jeflette.pencarigithub.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+private const val USER_PREFERENCES = "user_preferences"
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DataStoreModule {
+
+    @Singleton
+    @Provides
+    fun providePreferencesDataStore(@ApplicationContext appContext: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(produceFile = {
+            appContext.preferencesDataStoreFile(
+                USER_PREFERENCES
+            )
+        })
+    }
+}
