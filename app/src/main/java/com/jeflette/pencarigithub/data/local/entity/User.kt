01d511c6c35fa458e2c01a65c3eed44855ea8d16
dionpouw <aldion.sumampouw@gmail.com>
2023-03-13 13:53:11 +0800

    added entity annotation for room database

--- app/src/main/java/com/jeflette/pencarigithub/data/local/entity/User.kt
+++ app/src/main/java/com/jeflette/pencarigithub/data/local/entity/User.kt
@@ -1,14 +1,21 @@
 package com.jeflette.pencarigithub.data.local.entity
 
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Entity(tableName = "user")
+@Parcelize
 data class User(
-    @field:SerializedName("login")
-    val login: String? = null,
 
-    @field:SerializedName("avatar_url")
-    val avatarUrl: String? = null,
+    @PrimaryKey @ColumnInfo(name = "id") @field:SerializedName("id") val id: Int? = null,
 
-    @field:SerializedName("id")
-    val id: Int? = null,
-)
+    @ColumnInfo(name = "user_name") @field:SerializedName("login") val login: String? = null,
+
+    @ColumnInfo(name = "avatar_url") @field:SerializedName("avatar_url") val avatarUrl: String? = null,
+
+    @ColumnInfo(name = "is_favorite") var isFavorite: Boolean = false
+) : Parcelable
