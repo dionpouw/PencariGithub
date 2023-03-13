151f9836838cdf739f31ef06737b9ad8a02b6a3d
dionpouw <aldion.sumampouw@gmail.com>
2023-03-13 13:56:04 +0800

    nothing changed

--- app/src/main/java/com/jeflette/pencarigithub/ui/followingsFollowersFragment/FollowingsFollowersFragment.kt
+++ app/src/main/java/com/jeflette/pencarigithub/ui/followingsFollowersFragment/FollowingsFollowersFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.jeflette.pencarigithub.data.local.entity.User
 import com.jeflette.pencarigithub.databinding.FragmentFollowingsFollowersBinding
 import com.jeflette.pencarigithub.ui.adapter.GithubAccountAdapter
 import com.jeflette.pencarigithub.utils.Resource
@@ -34,15 +35,16 @@ class FollowingsFollowersFragment : Fragment() {
         val sectionNumber = arguments?.getInt(ARG_SECTION_NUMBER)
         val username = arguments?.getString(ARG_USERNAME) ?: ""
 
-        if (sectionNumber == 1) viewModel.getFollowing(username) else viewModel.getFollowers(username)
+        if (sectionNumber == 1) viewModel.getFollowing(username) else viewModel.getFollowers(
+            username
+        )
         val userAdapter = GithubAccountAdapter()
 
         binding.rvUsers.apply {
             adapter = userAdapter
             layoutManager = LinearLayoutManager(context)
-            setHasFixedSize(false)
+            setHasFixedSize(true)
         }
-        userAdapter.notifyDataSetChanged()
 
         lifecycleScope.launchWhenStarted {
             viewModel.user.collectLatest {
@@ -50,7 +52,7 @@ class FollowingsFollowersFragment : Fragment() {
                     is Resource.Success -> {
                         binding.rvUsers.visibility = View.VISIBLE
                         binding.progressBar.visibility = View.GONE
-                        it.data?.let { users -> userAdapter.setUserList(users) }
+                        it.data?.let { users -> userAdapter.setData(users as ArrayList<User>) }
                     }
                     is Resource.Loading -> {
                         binding.rvUsers.visibility = View.GONE
