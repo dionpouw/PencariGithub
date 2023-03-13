2291a7cc0923bef5a8644ba0fc796c8837214738
dionpouw <aldion.sumampouw@gmail.com>
2023-03-13 13:55:41 +0800

    arrange repository for local, remote, and preference

--- app/src/main/java/com/jeflette/pencarigithub/data/GithubUserRepository.kt
+++ app/src/main/java/com/jeflette/pencarigithub/data/GithubUserRepository.kt
@@ -1,6 +1,8 @@
 package com.jeflette.pencarigithub.data
 
+import com.jeflette.pencarigithub.data.local.LocalDataSource
 import com.jeflette.pencarigithub.data.local.entity.User
+import com.jeflette.pencarigithub.data.preference.UserPreferencesRepository
 import com.jeflette.pencarigithub.data.remote.RemoteDataSource
 import com.jeflette.pencarigithub.data.remote.response.DetailUserResponse
 import com.jeflette.pencarigithub.data.remote.response.SearchUserResponse
@@ -10,7 +12,9 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GithubUserRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource,
+    private val userPreferencesRepository: UserPreferencesRepository
 ) {
     fun getSearchUsers(username: String): Flow<Resource<SearchUserResponse>> = flow {
         emit(Resource.Success(remoteDataSource.getSearchUsers(username)))
@@ -26,5 +30,25 @@ class GithubUserRepository @Inject constructor(
 
     fun getFollowers(username: String): Flow<Resource<List<User>>> = flow {
         emit(Resource.Success(remoteDataSource.getFollowers(username)))
+    }
+
+    fun getUsers(): Flow<Resource<List<User>>> = flow {
+        emit(Resource.Success(localDataSource.getUsers()))
+    }
+
+    suspend fun insertUser(user: User) {
+        localDataSource.insertUser(user)
+    }
+
+    suspend fun deleteUser(user: User) {
+        localDataSource.deleteUser(user)
+    }
+
+    fun getTheme(): Flow<Boolean> {
+        return userPreferencesRepository.readThemeMode()
+    }
+
+    suspend fun saveTheme(isDarkMode: Boolean) {
+        userPreferencesRepository.saveThemeMode(isDarkMode)
     }
 }
